9daf1d1c2b00367cf892fc6ecc1447e3991636c7
Abi Hendrick <hello@abigailhendrick.com>
2023-09-24 09:57:51 -0400

    finished

--- app/page.js
+++ app/page.js
@@ -2,111 +2,74 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen py-24 bg-gradient-to-b from-stone-300 to-teal-200">
+      <h1 className='text-center text-4xl font-serif'>The 10-5-3 Strategy</h1>
+      <div className='w-full flex flex-col items-center my-4'>
+        <div><strong>10</strong> Companies</div>
+        <div><strong>5</strong> Positions</div>
+        <div><strong>3</strong> Applications</div>
+      </div>
+      <div className='mt-12'>
+        <p className='text-xl text-gray font-serif text-center'>🧠Losing your mind on the job hunt?🧠</p>
+      </div>
+      <div className='px-48 mt-4'>
+        This is Abi&apos;s recommended job hunt strategy. It consists of three simple steps, two of which are research based.
+        The strategy is meant as a guideline to help candidates get started with job hunting and begin to understand
+        the market. It is not a guarantee. Instead, it is the way to approach the job hunt without losing your mind.
+      </div>
+      
+      <div>
+        <p className='text-xl text-gray font-serif text-center mt-12'>1️⃣Research 10 companies</p>
+      </div>
+      <div className='px-48 mt-4'>
+        The first step is to research 10 companies. Ask yourself the following questions.
+        <div className='ms-4 mt-2'>~ What size of company do I want to work for?</div>
+        <div className='ms-4'>~ What type of industries interest me?</div>
+        <div className='ms-4 mb-2'>~ What sort of company aligns with my values?</div>
+        There are many more questions to consider but these should get you started.
+        From there, begin your research! Find at least 10 companies that fit with the answers to your questions.
+      </div>
+
+      <div>
+        <p className='text-xl text-gray font-serif text-center mt-12'>2️⃣Find 5 positions</p>
+        <div className='px-48 mt-4'>
+          This is the easy step! Look at the job postings for your companies and pick out 5 that you
+          might be interested in applying for. Keep these in a list since you may use them later.
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      
+      <div>
+        <p className='text-xl text-gray font-serif text-center mt-12'>3️⃣Apply 3 times</p>
+        <div className='px-48 mt-4'>
+          This is the hard step. First, pick 3 of the job postings off your list. For each of those, do the following.
+          <div className='ms-4 mt-2'>~ Tailor <strong>every part</strong> of your resume to match the job posting.</div>
+          <div className='ms-4'>~ Create a custom cover letter for the job posting</div>
+          <div className='ms-4 mb-2'>~ Research the company on LinkedIn and message 2-3 people who work at the company.</div>
+        </div>
       </div>
+      
+      <div>
+        <p className='text-xl text-gray font-serif text-center mt-12'>❔Why this strategy❔</p>
+        <div className='px-48 mt-4'>
+          The reasoning for this strategy is hidden in each of the steps. The first step makes you figure out
+          what you want. The second step is the traditional job hunting part. The third step is the special sauce 
+          of this strategy. Folks looking at your resume have probably seen 100s of generic resumes. By tailoring
+          yours, you stand out as a beacon of hope to them that their candidate hunt is finally over. Then they see
+          your cover letter that is also customized for them and to them. This seals the deal. However, that does 
+          not always work. So now you need visibility in the company. That is why you want to reach out to the 
+          people working there. I know this is the scariest part so here is some bonus advice. This is
+          Abi&apos;s template for reaching out to people on LinkedIn in 300 characters or less!
+          <div className='ms-4 mt-2'>1. Intro and soft ask</div>
+          <div className='ms-4'>2. Background and connection point</div>
+          <div className='ms-4 mb-2'>3. Thanks and removal of obligation</div>
+          An example would be:
+          <div className='border-start border-gray-600 border-s-2 ps-4 m-4'>
+            I'm a front-end engineer and I saw that you do a lot of front-end work, would you be interested in telling me more about your career? I noticed you contributed to an open source project and that sounds really interesting to me! Thanks for reading this and obviously no pressure!
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p className='text-center font-serif text-xl mt-12'>!!!Thanks!!!</p>
+        <p className='px-48 mt-4'>Thanks for reading and best of luck to you! The job hunt can be exhausting but we can all get through this together.</p>
       </div>
     </main>
   )
